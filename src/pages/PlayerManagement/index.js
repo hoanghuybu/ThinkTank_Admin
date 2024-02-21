2aeb032f1c9d6e9aea1f205d9044e0aca4501c9f
hoanghuybu <dohoanghuy8a3@gmail.com>
2024-02-21 20:52:30 +0700

    feat: apply api account management

--- src/pages/PlayerManagement/index.js
+++ src/pages/PlayerManagement/index.js
@@ -1,104 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+//util
+import * as playerManagementService from '~/service/PlayerManagementService';
+
 import { Dropdown } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleLeft, faAngleRight, faFilter } from '@fortawesome/free-solid-svg-icons';
-import './UserManagement.scss';
+import './PlayerManagement.scss';
 import Search from '~/layouts/Search';
-
-const data = [
-    {
-        stt: 1,
-        image: 'https://play-lh.googleusercontent.com/ZvMvaLTdYMrD6U1B3wPKL6siMYG8nSTEnzhLiMsH7QHwQXs3ZzSZuYh3_PTxoU5nKqU',
-        name: 'John Doe',
-        birthday: '01/01/1990',
-        email: 'john@example.com',
-        gender: 'Male',
-    },
-    {
-        stt: 2,
-        image: 'https://play-lh.googleusercontent.com/ZvMvaLTdYMrD6U1B3wPKL6siMYG8nSTEnzhLiMsH7QHwQXs3ZzSZuYh3_PTxoU5nKqU',
-        name: 'Jane Smith',
-        birthday: '05/15/1985',
-        email: 'jane@example.com',
-        gender: 'Female',
-    },
-    {
-        stt: 3,
-        image: 'https://play-lh.googleusercontent.com/ZvMvaLTdYMrD6U1B3wPKL6siMYG8nSTEnzhLiMsH7QHwQXs3ZzSZuYh3_PTxoU5nKqU',
-        name: 'Bob Johnson',
-        birthday: '10/20/1995',
-        email: 'bob@example.com',
-        gender: 'Male',
-    },
-    {
-        stt: 4,
-        image: 'https://play-lh.googleusercontent.com/ZvMvaLTdYMrD6U1B3wPKL6siMYG8nSTEnzhLiMsH7QHwQXs3ZzSZuYh3_PTxoU5nKqU',
-        name: 'Alice Brown',
-        birthday: '03/08/1988',
-        email: 'alice@example.com',
-        gender: 'Female',
-    },
-    {
-        stt: 5,
-        image: 'https://play-lh.googleusercontent.com/ZvMvaLTdYMrD6U1B3wPKL6siMYG8nSTEnzhLiMsH7QHwQXs3ZzSZuYh3_PTxoU5nKqU',
-        name: 'Charlie Davis',
-        birthday: '07/12/1992',
-        email: 'charlie@example.com',
-        gender: 'Male',
-    },
-    {
-        stt: 6,
-        image: 'https://play-lh.googleusercontent.com/ZvMvaLTdYMrD6U1B3wPKL6siMYG8nSTEnzhLiMsH7QHwQXs3ZzSZuYh3_PTxoU5nKqU',
-        name: 'Eva White',
-        birthday: '12/25/1980',
-        email: 'eva@example.com',
-        gender: 'Female',
-    },
-    {
-        stt: 7,
-        image: 'https://play-lh.googleusercontent.com/ZvMvaLTdYMrD6U1B3wPKL6siMYG8nSTEnzhLiMsH7QHwQXs3ZzSZuYh3_PTxoU5nKqU',
-        name: 'David Wilson',
-        birthday: '09/18/1998',
-        email: 'david@example.com',
-        gender: 'Male',
-    },
-    {
-        stt: 8,
-        image: 'https://play-lh.googleusercontent.com/ZvMvaLTdYMrD6U1B3wPKL6siMYG8nSTEnzhLiMsH7QHwQXs3ZzSZuYh3_PTxoU5nKqU',
-        name: 'Grace Miller',
-        birthday: '06/30/1994',
-        email: 'grace@example.com',
-        gender: 'Female',
-    },
-    {
-        stt: 9,
-        image: 'https://play-lh.googleusercontent.com/ZvMvaLTdYMrD6U1B3wPKL6siMYG8nSTEnzhLiMsH7QHwQXs3ZzSZuYh3_PTxoU5nKqU',
-        name: 'Frank Taylor',
-        birthday: '11/05/1987',
-        email: 'frank@example.com',
-        gender: 'Male',
-    },
-    {
-        stt: 10,
-        image: 'https://play-lh.googleusercontent.com/ZvMvaLTdYMrD6U1B3wPKL6siMYG8nSTEnzhLiMsH7QHwQXs3ZzSZuYh3_PTxoU5nKqU',
-        name: 'Holly Anderson',
-        birthday: '04/22/1991',
-        email: 'holly@example.com',
-        gender: 'Female',
-    },
-];
+import { get } from '~/untils/request';
 
 function PlayerManagement() {
-    const itemsPerPage = 2;
     const [currentPage, setCurrentPage] = useState(1);
+    const [listPlayers, setListPlayer] = useState([]);
+
+    const itemsPerPage = 2;
 
     const startIndex = (currentPage - 1) * itemsPerPage;
     const endIndex = startIndex + itemsPerPage;
-    const currentData = data.slice(startIndex, endIndex);
+    const currentData = listPlayers.slice(startIndex, endIndex);
 
     const paginate = (pageNumber) => {
         setCurrentPage(pageNumber);
     };
+
+    const getListPlayers = async () => {
+        try {
+            const token = sessionStorage.getItem('accessToken');
+            const result = await playerManagementService.getListPlayers(null, null, token);
+            setListPlayer(result.results);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const handleBlockPlayer = async (accountId) => {
+        const token = sessionStorage.getItem('accessToken');
+        const result = await playerManagementService.banPlayers(accountId, token);
+        if (result) {
+            getListPlayers();
+        }
+    };
+
+    useEffect(() => {
+        getListPlayers();
+    }, []);
 
     return (
         <div className="content-wrapper">
@@ -127,19 +73,29 @@ function PlayerManagement() {
                             <tbody className="table-border-bottom-0">
                                 {currentData.map((item, index) => (
                                     <tr key={index}>
-                                        <td className="text-center">{startIndex + index + 1}</td>
+                                        <td className="text-center">{item?.code}</td>
                                         <td className="text-center">
                                             <img
                                                 className="column-image"
-                                                src={item.image}
+                                                // src={item.image}
+                                                src="https://play-lh.googleusercontent.com/ZvMvaLTdYMrD6U1B3wPKL6siMYG8nSTEnzhLiMsH7QHwQXs3ZzSZuYh3_PTxoU5nKqU"
                                                 alt={`Image of ${item.name}`}
                                             />
                                         </td>
-                                        <td>{item.name}</td>
-                                        <td>{item.birthday}</td>
-                                        <td>{item.email}</td>
-                                        <td>{item.gender}</td>
-                                        <td>Active</td>
+                                        <td>{item?.fullName}</td>
+                                        <td>{item?.dateOfBirth}</td>
+                                        <td>{item?.email}</td>
+                                        <td>{item?.gender}</td>
+                                        <td>
+                                            <span
+                                                className={`badge ${
+                                                    item?.status === true ? 'bg-label-success' : 'bg-label-danger'
+                                                } me-1`}
+                                            >
+                                                {item?.status === true ? 'Active' : 'InActive'}
+                                            </span>
+                                            {/* {item?.status === true ? 'Active' : 'InActive'} */}
+                                        </td>
                                         <td>
                                             <Dropdown>
                                                 <Dropdown.Toggle variant="secondary" id="dropdown-basic">
@@ -147,7 +103,9 @@ function PlayerManagement() {
                                                 </Dropdown.Toggle>
 
                                                 <Dropdown.Menu>
-                                                    <Dropdown.Item>Block</Dropdown.Item>
+                                                    <Dropdown.Item onClick={() => handleBlockPlayer(item.id)}>
+                                                        Block
+                                                    </Dropdown.Item>
                                                 </Dropdown.Menu>
                                             </Dropdown>
                                         </td>
@@ -159,7 +117,8 @@ function PlayerManagement() {
                 </div>
                 <div className="w-100 d-flex justify-content-between">
                     <h4 className="table-text mt-1">
-                        Showing {startIndex + 1} to {endIndex > data.length ? data.length : endIndex} of {data.length}
+                        Showing {startIndex + 1} to {endIndex > listPlayers.length ? listPlayers.length : endIndex} of{' '}
+                        {listPlayers.length}
                     </h4>
                     <div className="pagination">
                         <button
@@ -171,7 +130,7 @@ function PlayerManagement() {
                         </button>
                         <span className="mx-2">
                             <ul className="pagination d-flex align-items-center h-100">
-                                {Array(Math.ceil(data.length / itemsPerPage))
+                                {Array(Math.ceil(listPlayers.length / itemsPerPage))
                                     .fill()
                                     .map((_, index) => (
                                         <li
@@ -188,7 +147,7 @@ function PlayerManagement() {
                         <button
                             className="btn-pagi"
                             onClick={() => setCurrentPage(currentPage + 1)}
-                            disabled={endIndex >= data.length}
+                            disabled={endIndex >= listPlayers.length}
                         >
                             <FontAwesomeIcon icon={faAngleRight}></FontAwesomeIcon>
                         </button>
--- src/service/PlayerManagementService.js
+++ src/service/PlayerManagementService.js
@@ -1,11 +1,27 @@
 import * as request from '~/untils/request';
 
-export const getListPlayers = async (Page, PageSize) => {
+export const getListPlayers = async (Page, PageSize, token) => {
     try {
-        const res = await request.get('/accounts', {
+        const res = await request.getApi('/accounts', {
             params: {
                 Page,
                 PageSize,
+            },
+            headers: {
+                Authorization: 'Bearer ' + token,
+            },
+        });
+        return res.data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const banPlayers = async (accountId, token) => {
+    try {
+        const res = await request.getApi(`/accounts/${accountId}/account-banned`, {
+            headers: {
+                Authorization: 'Bearer ' + token,
             },
         });
         return res.data;
